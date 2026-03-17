136627e7baac68f30e8e99a866702cffd628eb81
Maximiliano Vilariño <mvilarino@aldaba.es>
2026-03-17 10:10:37 +0100

    Refactor code structure for improved readability and maintainability

---
+++ assets/QrcodeAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace humhub\modules\qrcode\assets;
+
+use yii\web\AssetBundle;
+
+class QrcodeAsset extends AssetBundle
+{
+    public $sourcePath = '@qrcode/assets';
+    public $css = [];
+    public $js = [];
+}
